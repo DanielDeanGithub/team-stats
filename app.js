ba25db4ca782e478aa35da37a1114f820fb4cf8f
DanielDeanGithub <github-new@inboxrefresh.com>
2024-02-22 09:05:14 +0000

    Updated formatting of _players property

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const team = {
-    _players: [{ firstName: 'Isabella', lastName: 'Turner', age: '28'},
-                { firstName: 'Olivia', lastName: 'Foster', age: '29'},
-                { firstName: 'Lily', lastName: 'Carter', age: '32'}],
-    _games: [],
+  _players: [
+    { firstName: 'Isabella', lastName: 'Turner', age: 28 },
+    { firstName: 'Olivia', lastName: 'Foster', age: 29 },
+    { firstName: 'Lily', lastName: 'Carter', age: 32 },
+  ],
+  _games: [],
 };
