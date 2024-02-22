c5ca8c4fc0e1478dae8304846404b9def710afde
DanielDeanGithub <github-new@inboxrefresh.com>
2024-02-22 08:59:58 +0000

    Populated empty _player array with three player objects

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const team = {
-    _players: [],
+    _players: [{ firstName: 'Isabella', lastName: 'Turner', age: '28'},
+                { firstName: 'Olivia', lastName: 'Foster', age: '29'},
+                { firstName: 'Lily', lastName: 'Carter', age: '32'}],
     _games: [],
 };
