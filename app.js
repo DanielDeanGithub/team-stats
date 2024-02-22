a3f5de86c72841502fa472763dd401a6a19906b4
DanielDeanGithub <github-new@inboxrefresh.com>
2024-02-22 09:28:50 +0000

    Tested addGame method and printed output to console

--- app.js
+++ app.js
@@ -1,29 +1,40 @@
 const team = {
-    _players: [
-        { firstName: 'Isabella', lastName: 'Turner', age: 28 },
-        { firstName: 'Olivia', lastName: 'Foster', age: 29 },
-        { firstName: 'Lily', lastName: 'Carter', age: 32 },
-    ],
-    _games: [
-        { opponent: 'Nebula Ninjas', teamPoints: 20, opponentPoints: 17 },
-        { opponent: 'Sapphire Sisters', teamPoints: 19, opponentPoints: 23 },
-        { opponent: 'Celestial Challengers', teamPoints: 26, opponentPoints: 18 }
-    ],
-    get players() {
-        return this._players;
-    },
-    get games() {
-        return this._games;
-    },
-    addPlayer(newFirstName, newLastName, newAge) {
-        let player = { firstName: newFirstName, lastName: newLastName, age: newAge }
-        this.players.push(player);
-    },
-    addGame(newOpponent, newTeamPoints, newOpponentPoints) {
-        let game = { opponent: newOpponent, teamPoints: newTeamPoints, opponentPoints: newOpponentPoints }
-        this.games.push(game);
-    }
+  _players: [
+    { firstName: "Isabella", lastName: "Turner", age: 28 },
+    { firstName: "Olivia", lastName: "Foster", age: 29 },
+    { firstName: "Lily", lastName: "Carter", age: 32 },
+  ],
+  _games: [
+    { opponent: "Nebula Ninjas", teamPoints: 20, opponentPoints: 17 },
+    { opponent: "Sapphire Sisters", teamPoints: 19, opponentPoints: 23 },
+    { opponent: "Celestial Challengers", teamPoints: 26, opponentPoints: 18 },
+  ],
+  get players() {
+    return this._players;
+  },
+  get games() {
+    return this._games;
+  },
+  addPlayer(newFirstName, newLastName, newAge) {
+    let player = {
+      firstName: newFirstName,
+      lastName: newLastName,
+      age: newAge,
+    };
+    this.players.push(player);
+  },
+  addGame(newOpponent, newTeamPoints, newOpponentPoints) {
+    let game = {
+      opponent: newOpponent,
+      teamPoints: newTeamPoints,
+      opponentPoints: newOpponentPoints,
+    };
+    this.games.push(game);
+  },
 };
 
-team.addPlayer('Bugs', 'Bunny', 76);
+team.addPlayer("Bugs", "Bunny", 76);
 console.log(team.players);
+
+team.addGame("Titans", 100, 98);
+console.log(team.games);
