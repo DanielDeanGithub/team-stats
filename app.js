c731cfde5bbe73630ff5f7e41bc135441e87660c
DanielDeanGithub <github-new@inboxrefresh.com>
2024-02-22 09:09:58 +0000

    Populated empty _games array with 3 game objects

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const team = {
-  _players: [
-    { firstName: 'Isabella', lastName: 'Turner', age: 28 },
-    { firstName: 'Olivia', lastName: 'Foster', age: 29 },
-    { firstName: 'Lily', lastName: 'Carter', age: 32 },
-  ],
-  _games: [],
+    _players: [
+        { firstName: 'Isabella', lastName: 'Turner', age: 28 },
+        { firstName: 'Olivia', lastName: 'Foster', age: 29 },
+        { firstName: 'Lily', lastName: 'Carter', age: 32 },
+    ],
+    _games: [
+        { opponent: 'Nebula Ninjas', teamPoints: 20, opponentPoints: 17 },
+        { opponent: 'Sapphire Sisters', teamPoints: 19, opponentPoints: 23 },
+        { opponent: 'Celestial Challengers', teamPoints: 26, opponentPoints: 18 }
+    ],
 };
